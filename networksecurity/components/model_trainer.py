81301d686b881e19de92502b490c54139a0f748f
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-12-12 11:44:37 +0545

    minor changes

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -117,6 +117,8 @@ class ModelTrainer:
         os.makedirs(model_dir_path, exist_ok=True)
         
         save_object(self.model_trainer_config.trained_model_file_path, network_model)
+        
+        save_object("final_model/model.pkl", best_model)
 
         ## Model Trainer Artifact
         model_trainer_artifact = ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
